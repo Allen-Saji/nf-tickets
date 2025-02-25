3d2484982cc1f2650f59bb16b575b076ee47f4ec
Allen-Saji <allensaji04@gmail.com>
2025-02-25 20:11:24 +0530

    update navbar ui

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,34 +1,45 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Search, ChevronDown } from "lucide-react"; // For search and dropdown arrow
+import { Search, Menu, ChevronDown } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { ShimmerButton } from "./magicui/shimmer-button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetClose,
+} from "@/components/ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState(false); // State for dropdown
-  const { data: session } = useSession(); // Destructure session data
-  const dropdownRef = useRef<HTMLDivElement>(null); // Explicitly set the type
+  const { data: session } = useSession();
+  const [placeholder, setPlaceholder] = useState("events");
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
-  const handleLogout = async () => {
-    await signOut({ callbackUrl: "/" }); // Redirect to root after sign out
-  };
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen); // Toggle dropdown visibility
-  };
-
-  // Handle click outside to close dropdown
   useEffect(() => {
-    const handleClickOutside = (event: any) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setDropdownOpen(false);
-      }
-    };
+    const texts = ["events", "artists", "venues"];
+    let currentIndex = 0;
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
+    const intervalId = setInterval(() => {
+      setIsTransitioning(true);
+      setTimeout(() => {
+        currentIndex = (currentIndex + 1) % texts.length;
+        setPlaceholder(texts[currentIndex]);
+        setIsTransitioning(false);
+      }, 500);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   useEffect(() => {
@@ -43,16 +54,14 @@ const Navbar = () => {
   return (
     <nav
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-        isScrolled
-          ? "bg-black bg-opacity-50 backdrop-filter backdrop-blur-lg"
-          : "bg-black"
-      }`}
+        isScrolled ? "bg-black bg-opacity-50 backdrop-blur-lg" : "bg-black"
+      } `}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <Link href="/" className="flex-shrink-0 mr-8">
-              <div className="relative w-16 h-16">
+      <div className="w-full max-w-full px-6 sm:px-10 lg:px-14">
+        <div className="flex items-center justify-between h-20">
+          <div className="flex-none">
+            <Link href="/" className="flex-shrink-0">
+              <div className="relative w-14 h-14 sm:w-16 sm:h-16">
                 <Image
                   src="/logo.png"
                   alt="Logo"
@@ -62,87 +71,124 @@ const Navbar = () => {
                 />
               </div>
             </Link>
-            <div className="hidden md:flex space-x-6">
+          </div>
+
+          <div className="hidden md:flex flex-1 justify-center items-center">
+            <div className="flex items-center space-x-8 mx-auto">
               <Link
                 href="/"
-                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-white hover:text-[#DEFF58] px-4 py-2 rounded-md text-lg font-semibold"
               >
                 Home
               </Link>
               <Link
                 href="/events"
-                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-white hover:text-[#DEFF58] px-4 py-2 rounded-md text-lg font-semibold"
               >
                 Events
               </Link>
-            </div>
-          </div>
-          <div className="flex-1 max-w-md px-4">
-            <div className="relative">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <Search className="h-5 w-5 text-gray-400" />
+              <div className="relative flex items-center w-72">
+                <Search className="absolute left-3 h-6 w-6 text-gray-400" />
+                <Input
+                  type="text"
+                  className="pl-12 py-3 h-12 w-full bg-gray-800 text-gray-300 border-transparent focus:ring-0 rounded-full focus:bg-gray-700 focus:text-gray-100 text-lg"
+                  placeholder={`Search for ${placeholder}`}
+                />
               </div>
-              <input
-                type="text"
-                className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-full leading-5 bg-gray-700 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out"
-                placeholder="Search events, artists..."
-              />
             </div>
           </div>
-          <div className="flex items-center">
+
+          <div className="flex items-center space-x-4">
             {session?.user ? (
-              <div className="relative ml-4" ref={dropdownRef}>
-                <button
-                  className="flex items-center text-white focus:outline-none"
-                  onClick={toggleDropdown}
-                >
-                  <div className="relative w-8 h-8">
-                    <Image
-                      src={session.user.image || "/default-avatar.png"}
-                      alt="User Avatar"
-                      fill
-                      className="rounded-full object-cover"
-                    />
-                  </div>
-                  <span className="ml-2 text-sm font-medium">
-                    {session.user.name}
-                  </span>
-                  <ChevronDown className="ml-1 h-4 w-4" />
-                </button>
-                {dropdownOpen && (
-                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-10">
-                    {session.user.ArtistProfile ? (
-                      <Link
-                        href="/artist/profile"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      >
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="p-0 h-auto">
+                    <div className="flex items-center">
+                      <div className="relative w-10 h-10 sm:w-12 sm:h-12">
+                        <Image
+                          src={session.user.image || "/default-avatar.png"}
+                          alt="User Avatar"
+                          fill
+                          className="rounded-full object-cover"
+                        />
+                      </div>
+                      <span className="ml-2 text-lg font-medium text-white hidden sm:inline">
+                        {session.user.name}
+                      </span>
+                      <ChevronDown className="ml-2 h-5 w-5 text-white" />
+                    </div>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  {session.user.ArtistProfile ? (
+                    <DropdownMenuItem asChild>
+                      <Link href="/artist/profile">
                         Switch to Artist Profile
                       </Link>
-                    ) : (
-                      <Link
-                        href="/artist/create"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      >
-                        Create Artist Profile
-                      </Link>
-                    )}
-                    <button
-                      onClick={handleLogout}
-                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    >
-                      Logout
-                    </button>
-                  </div>
-                )}
-              </div>
+                    </DropdownMenuItem>
+                  ) : (
+                    <DropdownMenuItem asChild>
+                      <Link href="/artist/create">Create Artist Profile</Link>
+                    </DropdownMenuItem>
+                  )}
+                  <DropdownMenuItem
+                    onClick={() => signOut({ callbackUrl: "/" })}
+                  >
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             ) : (
-              <button
-                className="ml-4 px-4 py-2 rounded-md text-sm font-medium text-black bg-[#DEFF58] hover:bg-opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              <ShimmerButton
                 onClick={() => signIn("google")}
+                className="h-12 px-6 py-3 text-lg font-semibold hover:text-[#DEFF58]"
               >
                 Login
-              </button>
+              </ShimmerButton>
             )}
+
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="md:hidden text-white p-2"
+                >
+                  <Menu className="h-8 w-8" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent
+                side="right"
+                className="bg-gray-900 text-white border-gray-800"
+              >
+                <div className="flex flex-col h-full space-y-6">
+                  <div className="relative mt-4">
+                    <Search className="absolute left-4 top-1/2 -translate-y-1/2 h-6 w-6 text-gray-400" />
+                    <Input
+                      type="text"
+                      className="pl-14 py-3 bg-gray-800 text-gray-300 border-transparent focus:ring-0 rounded-full"
+                      placeholder={`Search for ${placeholder}`}
+                    />
+                  </div>
+                  <SheetClose asChild>
+                    <Link
+                      href="/"
+                      className="block px-6 py-3 rounded-md text-xl font-semibold hover:bg-gray-800"
+                    >
+                      Home
+                    </Link>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Link
+                      href="/events"
+                      className="block px-6 py-3 rounded-md text-xl font-semibold hover:bg-gray-800"
+                    >
+                      Events
+                    </Link>
+                  </SheetClose>
+                </div>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </div>
