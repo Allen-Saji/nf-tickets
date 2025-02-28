2e275efc0fa5547e5da315c56a0026b261de35cb
Allen-Saji <allensaji04@gmail.com>
2025-03-01 00:31:18 +0530

    change sonner styling

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,12 +1,11 @@
-"use client"
+"use client";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
 
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
-
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
@@ -15,8 +14,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
+            "group toast group-[.toaster]:bg-lime-400 group-[.toaster]:text-black group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "!text-black dark:!text-black", // Force black text with high specificity
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
@@ -25,7 +24,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
